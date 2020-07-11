ea5c414c097a4adb6c15a6bacb15fca2fa097a29
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-07-11 15:44:43 +0700

    new

--- resources/views/admin/lessons/create.blade.php
+++ resources/views/admin/lessons/create.blade.php
@@ -262,6 +262,50 @@ Dropzone.options.downloadableFileDropzone = {
 <script>
     // Replace the <textarea id="editor1"> with a CKEditor 4
     // instance, using default configuration.
-    CKEDITOR.replace( 'full_text' );
+    if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
+    CKEDITOR.tools.enableHtml5Elements( document );
+
+// The trick to keep the editor in the sample quite small
+// unless user specified own height.
+CKEDITOR.config.height = 150;
+CKEDITOR.config.width = 'auto';
+
+var initSample = ( function() {
+    var wysiwygareaAvailable = isWysiwygareaAvailable(),
+        isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
+
+    return function() {
+        var editorElement = CKEDITOR.document.getById( 'full_text' );
+
+        // :(((
+        if ( isBBCodeBuiltIn ) {
+            editorElement.setHtml(
+                'Hello world!\n\n' +
+                'I\'m an instance of [url=https://ckeditor.com]CKEditor[/url].'
+            );
+        }
+
+        // Depending on the wysiwygarea plugin availability initialize classic or inline editor.
+        if ( wysiwygareaAvailable ) {
+            CKEDITOR.replace( 'full_text' );
+        } else {
+            editorElement.setAttribute( 'contenteditable', 'true' );
+            CKEDITOR.inline( 'full_text' );
+
+            // TODO we can consider displaying some info box that
+            // without wysiwygarea the classic editor may not work.
+        }
+    };
+
+    function isWysiwygareaAvailable() {
+        // If in development mode, then the wysiwygarea must be available.
+        // Split REV into two strings so builder does not replace it :D.
+        if ( CKEDITOR.revision == ( '%RE' + 'V%' ) ) {
+            return true;
+        }
+
+        return !!CKEDITOR.plugins.get( 'wysiwygarea' );
+    }
+} )();
 </script>
 @stop
